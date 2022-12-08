21cbb6382153c3e8e745707e2f5daac5dc7b0ca7
Amit Kulkarni <akulkarni334@gatech.edu>
2022-12-08 13:44:24 -0500

    add separate addAxes() function

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
 // Global function called when the button is clicked
 function onCategoryChanged() {
+    // console.log("--------------------");
+    // // print out all of the modes of transportation that are selected
+    // console.log("Cars selected: " + d3.select("#cars").property("checked"));
+    // console.log("Pedestrians selected: " + d3.select("#pedestrians").property("checked"));
+    // console.log("Motorcycles selected: " + d3.select("#motorcycles").property("checked"));
+    // console.log("Bicycles selected: " + d3.select("#bicycles").property("checked"));
+    // console.log("Trucks selected: " + d3.select("#trucks").property("checked"));
+    // // print out if yearly or 100k is selected
+    // console.log("\nYearly selected: " + d3.select("#yearly").property("checked"));
+    // console.log("100k selected: " + d3.select("#hundredk").property("checked"));
+    // console.log("You clicked the button!")
+    // console.log("--------------------");
 
+    // create an array of the modes of transportation that are selected
+    var modes = [];
+    if (d3.select("#cars").property("checked")) {
+        modes.push("Car_Occupant");
+    }
+    if (d3.select("#pedestrians").property("checked")) {
+        modes.push("Pedestrian");
+    }
+    if (d3.select("#motorcycles").property("checked")) {
+        modes.push("Motorcycle");
+    }
+    if (d3.select("#bicycles").property("checked")) {
+        modes.push("Bicycle");
+    }
+    if (d3.select("#trucks").property("checked")) {
+        modes.push("Trucks");
+    }
+
+    console.log(modes);
+    updateChart(modes)
 }
 
 var width = 600;
 var height = 400;
 
-d3.csv("datasets/TransportationFatalities_ByYear_postoncanvas.csv", function (csv) {
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    // Axis setup
-    var xScale = d3.scaleTime().domain(d3.extent(csv, function(d) {
-        // console.log(d.Year)
-        return d3.timeParse("%Y")(d.Year);
-    })).range([50, width-30]);
-    var yScale = d3.scaleLinear().domain([0, (d3.max(csv, function(d) {
-        return d.Car_Occupant;
-    }))]).range([height-30, 30]);
-    
-    var xAxis = d3.axisBottom().scale(xScale);
-    var yAxis = d3.axisLeft().scale(yScale);
+// scales
+var xScale;
+var yScale;
 
-    //Create SVGs for charts
+// chart1 = d3
+// .select("#chart1")
+// .append("svg:svg")
+// .attr("id", "svg1")
+// .attr("width", width)
+// .attr("height", height);
+
+d3.csv("datasets/TransportationFatalities_ByYear_postoncanvas.csv", function (csv) {
     var chart1 = d3
     .select("#chart1")
     .append("svg:svg")
@@ -28,27 +57,7 @@ d3.csv("datasets/TransportationFatalities_ByYear_postoncanvas.csv", function (cs
     .attr("width", width)
     .attr("height", height);
 
-    // Create labels for the chart
-
-    chart1 // or something else that selects the SVG element in your visualizations
-    .append("g") // create a group node
-    .attr("transform", "translate(0," + (height - 30) + ")")
-    .call(xAxis) // call the axis generator
-    .append("text")
-    .attr("class", "label")
-    .attr("x", width - 16)
-    .attr("y", -6)
-    .style("text-anchor", "end")
-
-    chart1 // or something else that selects the SVG element in your visualizations
-    .append("g") // create a group node
-    .attr("transform", "translate(50, 0)")
-    .call(yAxis)
-    .append("text")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end");
-
+    addAxes(csv);
 
     // append the car data points to the chart as a line graph
     chart1.append("path")
@@ -121,47 +130,64 @@ d3.csv("datasets/TransportationFatalities_ByYear_postoncanvas.csv", function (cs
         }))
 
     // // Add filter button
-    d3.select('#chart2')
+    d3.select('.options')
         .append('p')
         .append('button')
         .attr('id', 'filterButton')
         .style("border", "1px solid black")
         .text('Filter Data')
         .on('click', function() {
-            console.log("--------------------");
-            // print out all of the modes of transportation that are selected
-            console.log("Cars selected: " + d3.select("#cars").property("checked"));
-            console.log("Pedestrians selected: " + d3.select("#pedestrians").property("checked"));
-            console.log("Motorcycles selected: " + d3.select("#motorcycles").property("checked"));
-            console.log("Bicycles selected: " + d3.select("#bicycles").property("checked"));
-            console.log("Trucks selected: " + d3.select("#trucks").property("checked"));
-            // print out if yearly or 100k is selected
-            console.log("\nYearly selected: " + d3.select("#yearly").property("checked"));
-            console.log("100k selected: " + d3.select("#hundredk").property("checked"));
-            console.log("You clicked the button!")
-            console.log("--------------------");
+            onCategoryChanged();
         });
 
 
 });
 
-function onFilterChanged() {
-    var carSelect = d3.select("#cars");
-    // Check to see if carSelect is checked
-    if (carSelect.property("checked", true)) {
-        // If it is, filter the data to only include cars
-        console.log("Cars selected");
-    }
+function addAxes(transit) {
+    // Axis setup
+    xScale = d3.scaleTime().domain(d3.extent(transit, function(d) { 
+        // console.log(d.Year)
+        return d3.timeParse("%Y")(d.Year);
+    })).range([50, width-30]);
+    yScale = d3.scaleLinear().domain([0, (d3.max(transit, function(d) {
+        return d.Car_Occupant;
+    }))]).range([height-30, 30]);
 
+    var xAxis = d3.axisBottom().scale(xScale);
+    var yAxis = d3.axisLeft().scale(yScale);
 
-    var pedestrianSelect = d3.select("#manufacturer").property("checked");
-    var motorCycleSelect = d3.select("#cutoff").property("checked");
-    var bicycleSelect = d3.select("#bicycle").property("checked");
-    var truckSelect = d3.select("#truck").property("checked");
+    // Create labels for the chart
 
+    var chart1 = d3.select('#chart1').select('svg');
+    var graph = chart1.selectAll('.y-axis').data(transit)
+    var chartGEnter = graph.enter()
+
+    // append x-axis
+    chart1 // or something else that selects the SVG element in your visualizations
+    .append("g") // create a group node
+    .attr("class", "axis-label x-axis")
+    .attr("transform", "translate(0," + (height - 30) + ")")
+    .call(xAxis) // call the axis generator
+    .append("text")
+    .attr("x", width - 16)
+    .attr("y", -6)
+    .style("text-anchor", "end")
+
+    chartGEnter 
+    .append("g") // create a group node
+    .attr("class", "axis-label y-axis")
+    .attr("transform", "translate(50, 0)")
+    .merge(chart1.selectAll(".y-axis"))
+    .data(transit)
+    .call(yAxis)
+    .append("text")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end");
+
+    graph.exit().remove();
 }
 
-// Create an update function for the axis that will scale the axis based on the data
-function addAxes(transportation) {
-
+function updateChart(modes) {
+    
 }
